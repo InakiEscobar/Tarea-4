ef3396b06ac2fc9103734f345d52572e26d4cf4a
Inaki <inaki.escobar@ug.uchile.cl>
2024-06-11 19:03:27 -0400

    "agregar función para sumas.py"

---
+++ sumas.py
@@ -0,0 +1,8 @@
+def sumas():
+    x = input("Ingrese un número: ")
+    y = input("Ingrese el segundo número que quiere sumar: ")
+    x1 = int(x)
+    y2 = int(y)
+    a = x1+y2
+    return "El resultado es: "+ str(a)
+print(sumas())
